22e104427c5c7ea6559d5c65982f8eb6b01a3356
Mbungai-Francesco <mbungaifrancesco2@gmail.com>
2024-03-21 15:45:23 +0100

    Scroll at project modified

--- Js/projects.js
+++ Js/projects.js
@@ -1,5 +1,17 @@
 const github = document.querySelector('.github')
+const follow = document.querySelector('.follower')
 
 github.addEventListener('click', function (){
   window.open('https://github.com/Mbungai-Francesco','_blank')
 })
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX + document.documentElement.scrollLeft;
+  const y = event.clientY + document.documentElement.scrollTop;
+  // console.log(`Mouse position: X=${x}, Y=${y}`)
+  follow.style.transform = `translate(${x-32}px, ${y-32}px)`;
+  // console.log(`${x1}, ${y1}`)
+  // if(Math.abs(x1-(x-32)) > 100 || Math.abs(y1-(y-32)) > 100){
+  //   block.style.transform = `translate(${50}vh, ${50}vh)`;
+  // }
+});
--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const resume = document.querySelector('.resume')
+const follow = document.querySelector('.follower')
 
 resume.addEventListener('click', function (){
   window.open('./MyCV.pdf','_blank')
 })
+
+document.addEventListener("mousemove", (event) => {
+  const x = event.clientX + document.documentElement.scrollLeft;
+  const y = event.clientY + document.documentElement.scrollTop;
+  follow.style.transform = `translate(${x-32}px, ${y-32}px)`;
+});
