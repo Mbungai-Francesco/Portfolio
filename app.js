b9695751b484a6f04432a013ca0900ca37841067
chrisvalere03gmail.com <nz-valere>
2024-04-22 13:15:41 +0100

    follower out

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ resume.addEventListener('click', function (){
   window.open('./MyCV.pdf','_blank')
 })
 
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX + document.documentElement.scrollLeft;
-  const y = event.clientY + document.documentElement.scrollTop;
-  follow.style.transform = `translate(${x-32}px, ${y-32}px)`;
-});
+// document.addEventListener("mousemove", (event) => {
+//   const x = event.clientX + document.documentElement.scrollLeft;
+//   const y = event.clientY + document.documentElement.scrollTop;
+//   follow.style.transform = `translate(${x-32}px, ${y-32}px)`;
+// });
